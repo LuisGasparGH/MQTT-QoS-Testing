58ccdad59b4dc5bd7e8db6f7aeb4ed05609da0a3
LuisGasparGH <luismgaspar01@gmail.com>
2023-04-23 19:22:24 +0100

    added timestamp logging on both server and client code (to be tested yet)

--- src/client.py
+++ src/client.py
@@ -12,31 +12,52 @@ with open("conf/config.json", "r") as config_file:
 
 # Stores all static variables needed
 client_id = str(sys.argv[1])
-log_folder = config['log_folder']
-log_name = log_folder + client_id + ".log"
+log_folder = config['logging']['folder']
+main_logger = config['logging']['main']
+timestamp_logger = config['logging']['timestamp']
+pyshark_logger = config['logging']['pyshark']
 broker_address = config['broker_address']
 main_topic = config['topics']['main_topic']
 begin_client = config['topics']['begin_client']
 finish_client = config['topics']['finish_client']
 client_done = config['topics']['client_done']
 
-# Configures the logger which will contain all execution details for further analysis
-logging.basicConfig(filename = log_name, filemode = 'a', format = '%(asctime)s %(levelname)s: %(message)s', level = logging.INFO)
-
 # Class of the client code
 class MQTT_Client:
+    # Configures the logger which will contain all execution details for further analysis
+    def logger_setup(self):
+        formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+        main_log = log_folder + client_id + "-main.log"
+        timestamp_log = log_folder + client_id + "-timestamp.log"
+        pyshark_log = log_folder + client_id + "-pyshark.log"
+        main_handler = logging.FileHandler(main_log, mode = 'a')
+        timestamp_handler = logging.FileHandler(timestamp_log, mode = 'a')
+        pyshark_handler = logging.FileHandler(pyshark_log, mode = 'a')
+        main_handler.setFormatter(formatter)
+        timestamp_handler.setFormatter(formatter)
+        pyshark_handler.setFormatter(formatter)
+        self.main_logger = logging.getLogger(main_logger)
+        self.timestamp_logger = logging.getLogger(timestamp_logger)
+        self.pyshark_logger = logging.getLogger(pyshark_logger)
+        self.main_logger.setLevel(logging.INFO)
+        self.timestamp_logger.setLevel(logging.INFO)
+        self.pyshark_logger.setLevel(logging.INFO)
+        self.main_logger.addHandler(main_handler)
+        self.timestamp_logger.addHandler(timestamp_handler)
+        self.pyshark_logger.addHandler(pyshark_handler)
+
     # Callback for when the client object successfully connects to the broker
     def on_connect(self, client, userdata, flags, rc):
         if rc==0:
             # Upon successful connection, the client subscribes to the start topic waiting for order
-            logging.info(f"Connected to the broker at {broker_address}")
+            self.main_logger.info(f"Connected to the broker at {broker_address}")
             self.client.subscribe(begin_client, qos=0)
-            logging.info(f"Subscribed to {begin_client} topic with QoS 0")
+            self.main_logger.info(f"Subscribed to {begin_client} topic with QoS 0")
             self.client.subscribe(finish_client, qos=0)
-            logging.info(f"Subscribed to {finish_client} topic with QoS 0")
+            self.main_logger.info(f"Subscribed to {finish_client} topic with QoS 0")
         else:
             # In case of error during connection the log will contain the error code for debugging
-            logging.info(f"Error connecting to broker, with code {rc}")
+            self.main_logger.info(f"Error connecting to broker, with code {rc}")
 
     # Callback for when the client object receives a message, which includes topic and payload
     def on_message(self, client, userdata, msg):
@@ -45,8 +66,8 @@ class MQTT_Client:
             self.cleanup()
         # If not, the topic will be the begin client, and the message will contain configuration information for the next run, such as message size, amount, publishing frequency and QoS level to be used
         elif str(msg.topic) == begin_client:
-            logging.warning(f"STARTING NEW RUN")
-            logging.info(f"Start order received from the server using topic {str(msg.topic)}")
+            self.main_logger.warning(f"STARTING NEW RUN")
+            self.main_logger.info(f"Start order received from the server using topic {str(msg.topic)}")
             # Declares the thread where the run handler will run. Has to be done everytime a new run is executed
             self.handler_thread = threading.Thread(target = self.run_handler, args = ())
             # Parses the payload to a dictionary, and stores all the needed settings
@@ -58,27 +79,27 @@ class MQTT_Client:
             self.sleep_time = (1/self.msg_freq)
             self.sent_counter = 0
             # Logs the run details and starts the run handler thread
-            logging.info(f"Message details: {self.msg_amount} messages of {self.msg_size} bytes using QoS level {self.msg_qos} with {self.msg_freq} Hz frequency")
+            self.main_logger.info(f"Message details: {self.msg_amount} messages of {self.msg_size} bytes using QoS level {self.msg_qos} with {self.msg_freq} Hz frequency")
             self.handler_thread.start()
 
     # Callback for the when the client object successfully completes the publishing of a message (including necessary handshake for QoS levels 1 and 2)
     def on_publish(self, client, userdata, mid):
         # The client contains a counter of sent messages for logging purposes
         self.sent_counter += 1
-        logging.info(f"Published message #{self.sent_counter} to the {main_topic} topic")
+        self.timestamp_logger.info(f"Published message #{self.sent_counter} to the {main_topic} topic (message id: {mid})")
         if self.sent_counter == self.msg_amount:
-            logging.info(f"Publish of all {self.msg_amount} messages complete")
+            self.main_logger.info(f"Publish of all {self.msg_amount} messages complete")
 
     # Callback for when the client object successfully disconnects from the broker
     def on_disconnect(self, client, userdata, rc):
-        logging.info(f"Disconnected from broker at {broker_address}")
+        self.main_logger.info(f"Disconnected from broker at {broker_address}")
 
     # Run handler function, used to execute each run with the information received from the server
     def run_handler(self):
         # As per the requirements, the client sleeps for 2 seconds after it receives the order to start, and after that creates the payload with the specific size for the run
         time.sleep(2)
         payload = bytearray(self.msg_size)
-        logging.info(f"Starting publish of {self.msg_amount} messages with QoS level {self.msg_qos}")
+        self.main_logger.info(f"Starting publish of {self.msg_amount} messages with QoS level {self.msg_qos}")
         # This cycle is iterated as many times as messages that need to be published in this run
         for msg in range(self.msg_amount):
             # Measures time of iteration start with a monotonic clock in order to precisely meet the frequency requirements
@@ -104,13 +125,14 @@ class MQTT_Client:
 
     # Starts the class with all the variables necessary
     def __init__(self):
-        logging.warning(f"NEW SYSTEM EXECUTION")
+        self.logger_setup()
+        self.main_logger.warning(f"NEW SYSTEM EXECUTION")
         self.msg_qos = 0
         self.msg_amount = 0
         self.msg_size = 0
         self.msg_freq = 0
         self.sent_counter = 0
-        logging.info(f"Creating MQTT Client with ID {client_id}")
+        self.main_logger.info(f"Creating MQTT Client with ID {client_id}")
         # Starts the MQTT client with specified ID, passed through the input arguments, and defines all callbacks
         self.client = mqtt.Client(client_id=client_id)
         self.client.on_connect = self.on_connect
--- src/server.py
+++ src/server.py
@@ -12,8 +12,10 @@ with open("conf/config.json", "r") as config_file:
 
 # Stores all static variables needed
 client_id = str(sys.argv[1])
-log_folder = config['log_folder']
-log_name = log_folder + client_id + ".log"
+log_folder = config['logging']['folder']
+main_logger = config['logging']['main']
+timestamp_logger = config['logging']['timestamp']
+pyshark_logger = config['logging']['pyshark']
 broker_address = config['broker_address']
 main_topic = config['topics']['main_topic']
 begin_client = config['topics']['begin_client']
@@ -21,20 +23,39 @@ finish_client = config['topics']['finish_client']
 client_done = config['topics']['client_done']
 system_runs = config['system_details']['different_runs']
 
-# Configures the logger, which will contain all execution details for further analysis
-logging.basicConfig(filename = log_name, filemode = 'a', format = '%(asctime)s %(levelname)s: %(message)s', level = logging.INFO)
-
 # Class of the server code
 class MQTT_Server:
+    # Configures the logger, which will contain all execution details for further analysis
+    def logger_setup(self):
+        formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+        main_log = log_folder + client_id + "-main.log"
+        timestamp_log = log_folder + client_id + "-timestamp.log"
+        pyshark_log = log_folder + client_id + "-pyshark.log"
+        main_handler = logging.FileHandler(main_log, mode = 'a')
+        timestamp_handler = logging.FileHandler(timestamp_log, mode = 'a')
+        pyshark_handler = logging.FileHandler(pyshark_log, mode = 'a')
+        main_handler.setFormatter(formatter)
+        timestamp_handler.setFormatter(formatter)
+        pyshark_handler.setFormatter(formatter)
+        self.main_logger = logging.getLogger(main_logger)
+        self.timestamp_logger = logging.getLogger(timestamp_logger)
+        self.pyshark_logger = logging.getLogger(pyshark_logger)
+        self.main_logger.setLevel(logging.INFO)
+        self.timestamp_logger.setLevel(logging.INFO)
+        self.pyshark_logger.setLevel(logging.INFO)
+        self.main_logger.addHandler(main_handler)
+        self.timestamp_logger.addHandler(timestamp_handler)
+        self.pyshark_logger.addHandler(pyshark_handler)
+
     # Callback for when the client object successfully connects to the broker
     def on_connect(self, client, userdata, flags, rc):
         if rc==0:
             # Upon successful connection, the system handler thread is started
-            logging.info(f"Connected to the broker at {broker_address}")
+            self.main_logger.info(f"Connected to the broker at {broker_address}")
             self.handler_thread.start()
         else:
             # In case of error during connection the log will contain the error code for debugging
-            logging.info(f"Error connecting to broker, with code {rc}")
+            self.main_logger.info(f"Error connecting to broker, with code {rc}")
 
     # Callback for when the client object receives a message, which includes topic and payload
     def on_message(self, client, userdata, msg):
@@ -45,6 +66,7 @@ class MQTT_Server:
                 self.run_start_time = time.monotonic()
             self.run_finish_time = time.monotonic()
             self.run_counter += 1
+            self.timestamp_logger.info(f"Received message #{self.run_counter} from the {main_topic} topic (message id: {msg.id})")
         # If the topic is the client done topic, it means that one of the clients has finished publishing of his messages
         elif str(msg.topic) == client_done:
             self.run_clients_done += 1
@@ -54,15 +76,15 @@ class MQTT_Server:
                 self.run_loss = 100-((self.run_counter/self.run_total_msg_amount)*100)
                 self.run_exec_time = self.run_finish_time-self.run_start_time
                 self.run_actual_freq = 1/(self.run_exec_time/(self.run_msg_amount-1))
-                logging.info(f"All {self.run_client_amount} clients finished publishing for this execution")
-                logging.info(f"Received {self.run_counter} out of {self.run_total_msg_amount} messages, totalling packet loss at {round(self.run_loss,3)}%")
-                logging.info(f"Total execution time was {round(self.run_exec_time,2)} seconds, totalling actual frequency at {round(self.run_actual_freq,2)} Hz")
+                self.main_logger.info(f"All {self.run_client_amount} clients finished publishing for this execution")
+                self.main_logger.info(f"Received {self.run_counter} out of {self.run_total_msg_amount} messages, totalling packet loss at {round(self.run_loss,3)}%")
+                self.main_logger.info(f"Total execution time was {round(self.run_exec_time,2)} seconds, totalling actual frequency at {round(self.run_actual_freq,2)} Hz")
                 self.client.unsubscribe(main_topic)
                 self.run_finished = True
 
     # Callback for when the client object successfully disconnects from the broker
     def on_disconnect(self, client, userdata, rc):
-        logging.info(f"Disconnected from broker at {broker_address}")
+        self.main_logger.info(f"Disconnected from broker at {broker_address}")
 
     # System handler function, used to feed all clients with each run information and start order. This function is run on a separate thread
     def sys_handler(self):
@@ -86,13 +108,13 @@ class MQTT_Server:
             # Subscribes to the message topic with the correct QoS to be used in the run, and logs all the information of the run, and also subscribes to the client done topic
             self.client.subscribe(main_topic, qos=self.run_msg_qos)
             self.client.subscribe(client_done, qos=0)
-            logging.warning(f"STARTING NEW RUN")
-            logging.info(f"Subscribed to {main_topic} topic with QoS level {self.run_msg_qos}")
-            logging.info(f"System details: {self.run_client_amount} clients with {self.run_msg_amount} messages each, with {self.run_msg_size} bytes sent at {self.run_msg_freq} Hz, using QoS level {self.run_msg_qos}, for a total of {self.run_total_msg_amount} messages")
+            self.main_logger.warning(f"STARTING NEW RUN")
+            self.main_logger.info(f"Subscribed to {main_topic} topic with QoS level {self.run_msg_qos}")
+            self.main_logger.info(f"System details: {self.run_client_amount} clients with {self.run_msg_amount} messages each, with {self.run_msg_size} bytes sent at {self.run_msg_freq} Hz, using QoS level {self.run_msg_qos}, for a total of {self.run_total_msg_amount} messages")
             # Dumps the information to a JSON payload to send to all the clients, and publishes it to the client topic
             client_config = json.dumps({"msg_qos": self.run_msg_qos, "msg_amount": self.run_msg_amount, "msg_size": self.run_msg_size, "msg_freq": self.run_msg_freq})
             self.client.publish(begin_client, client_config, qos=0)
-            logging.info(f"Sent configuration and start order to all the clients for run #{run+1}")
+            self.main_logger.info(f"Sent configuration and start order to all the clients for run #{run+1}")
             self.run_finished = False
             # This is used to wait for the previous run to finish and clean things up before starting a new one
             while self.run_finished == False:
@@ -109,7 +131,8 @@ class MQTT_Server:
 
     # Starts the class with all the variables necessary
     def __init__(self):
-        logging.warning(f"NEW SYSTEM EXECUTION")
+        self.logger_setup()
+        self.main_logger.warning(f"NEW SYSTEM EXECUTION")
         self.run_finished = True
         self.run_client_amount = 0
         self.run_msg_qos = 0
@@ -126,7 +149,7 @@ class MQTT_Server:
         self.run_clients_done = 0
         # Declares the thread where the system handler will run
         self.handler_thread = threading.Thread(target = self.sys_handler, args=())
-        logging.info(f"Creating MQTT Client with ID {client_id}")
+        self.main_logger.info(f"Creating MQTT Client with ID {client_id}")
         # Starts the MQTT client with specified ID, passed through the input arguments, and defines all callbacks
         self.client = mqtt.Client(client_id=client_id)
         self.client.on_connect = self.on_connect
