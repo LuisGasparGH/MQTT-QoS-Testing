83f6f1493df220a1c7d66dea13deabfdee4bed4b
LuisGasparGH <82114681+LuisGasparGH@users.noreply.github.com>
2024-08-29 10:10:39 +0100

    Modified update script to be server to client instead of client from server

--- scripts/update_source.sh
+++ scripts/update_source.sh
@@ -1,2 +1,12 @@
 #! /bin/bash
-scp -r upboard@192.168.2.3:/home/upboard/MQTT-QoS-Testing/src/ /home/ubuntu/MQTT-QoS-Testing/
+scp -r /home/upboard/MQTT-QoS-Testing/src/ ubuntu@192.168.2.10:/home/ubuntu/MQTT-QoS-Testing/
+scp -r /home/upboard/MQTT-QoS-Testing/src/ ubuntu@192.168.2.11:/home/ubuntu/MQTT-QoS-Testing/
+scp -r /home/upboard/MQTT-QoS-Testing/src/ ubuntu@192.168.2.12:/home/ubuntu/MQTT-QoS-Testing/
+scp -r /home/upboard/MQTT-QoS-Testing/src/ ubuntu@192.168.2.13:/home/ubuntu/MQTT-QoS-Testing/
+scp -r /home/upboard/MQTT-QoS-Testing/src/ ubuntu@192.168.2.14:/home/ubuntu/MQTT-QoS-Testing/
+scp -r /home/upboard/MQTT-QoS-Testing/src/ ubuntu@192.168.2.15:/home/ubuntu/MQTT-QoS-Testing/
+scp -r /home/upboard/MQTT-QoS-Testing/src/ ubuntu@192.168.2.16:/home/ubuntu/MQTT-QoS-Testing/
+scp -r /home/upboard/MQTT-QoS-Testing/src/ ubuntu@192.168.2.17:/home/ubuntu/MQTT-QoS-Testing/
+scp -r /home/upboard/MQTT-QoS-Testing/src/ ubuntu@192.168.2.18:/home/ubuntu/MQTT-QoS-Testing/
+scp -r /home/upboard/MQTT-QoS-Testing/src/ ubuntu@192.168.2.19:/home/ubuntu/MQTT-QoS-Testing/
+clear
